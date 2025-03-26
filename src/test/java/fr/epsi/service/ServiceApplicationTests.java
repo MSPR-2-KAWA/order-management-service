e7fd694c1b2cb583236b5788f2426d1e5485795a
LoanH <lheniart@be-infotech.fr>
2025-03-26 14:58:42 +0100

    first commit

---
+++ src/test/java/fr/epsi/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
