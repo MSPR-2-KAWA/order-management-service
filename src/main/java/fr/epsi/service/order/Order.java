57f7c6c3088a905a7e15aadb80b5cb6c481a1f88
BenBour62 <46033921+Arkoos62@users.noreply.github.com>
2025-06-18 10:52:50 +0200

    crud (controller + service + repo + dto) (#10)
    Co-authored-by: Benjamin Boursault <benjamin@inniti.fr>
    Co-authored-by: LoanH <lheniart@be-infotech.fr>

---
+++ src/main/java/fr/epsi/service/order/Order.java
@@ -0,0 +1,32 @@
+package fr.epsi.service.order;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    private Integer customerId;
+
+    public Order(Integer customerId) {
+        this.customerId = customerId;
+    }
+}
---
+++ src/main/java/fr/epsi/service/order/dto/OrderDTO.java
@@ -0,0 +1,11 @@
+package fr.epsi.service.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
+public class OrderDTO {
+
+    Integer customerId;
+}
